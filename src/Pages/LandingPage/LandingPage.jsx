d41ce831ed767aa869c575436ec46fa3ad5654a3
Jason Hargrove <rijaha@gmail.com>
2021-11-20 13:16:28 -0600

    Updated meta data and og image.

--- src/Pages/LandingPage/LandingPage.jsx
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -4,14 +4,23 @@ import './LandingPage.css';
 
 
 function LandingPage() {
-    return (
-        <div>
-            <LandingNav />
-            <h1 className="title">Groovy</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis voluptates, officiis assumenda! Dolor qui delectus eos sapiente, modi est doloremque aspernatur, alias odit earum? Dolorum quibusdam officia veritatis, maiores libero.</p>
-            <img src="" alt="coding" />
-        </div>
-    )
+  return (
+    <div>
+      <LandingNav />
+      <h1>Unwind With Color</h1>
+      <h2>A Digital Drawing Experience</h2>
+      <button>Go To Project</button>
+      <img src="/img/rainbow-splash-image.svg" alt="rainbow splash"/>
+
+      <h1>The Challenge</h1>
+
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam modi voluptas cum eum doloremque distinctio, non quia eaque cupiditate itaque incidunt soluta repellendus quod perferendis mollitia error tempora dignissimos. Cum.</p>
+
+      <h1>The Process</h1>
+
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At dolorem, maxime ipsam quas, alias labore, nulla eius quibusdam ratione voluptas, ab voluptatibus suscipit? Id, ipsa amet. Fugiat facere, similique commodi.</p>
+    </div>
+  )
 }
 
 export default LandingPage
