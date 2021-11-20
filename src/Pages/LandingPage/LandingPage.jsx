fcb2308919f7a179a301cfbb12c8788b212fc93c
Jason Hargrove <rijaha@gmail.com>
2021-11-20 13:53:39 -0600

    Added text and placeholders to LandingPage. Renamed and added images.

--- src/Pages/LandingPage/LandingPage.jsx
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import LandingNav from '../../Components/NavBars/LandingNav/LandingNav';
 import './LandingPage.css';
 
-
 function LandingPage() {
   return (
     <div>
@@ -19,6 +18,49 @@ function LandingPage() {
       <h1>The Process</h1>
 
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At dolorem, maxime ipsam quas, alias labore, nulla eius quibusdam ratione voluptas, ab voluptatibus suscipit? Id, ipsa amet. Fugiat facere, similique commodi.</p>
+
+      <img src="" alt="GitHub"/>
+      <img src="" alt="CodeSandbox"/>
+
+      <h1>The Technologies</h1>
+
+      <img src="" alt="React"/>
+      <img src="" alt="Bootstrap"/>
+      <h2>Front End</h2>
+
+      <img src="" alt="CSS"/>
+      <img src="" alt="?"/>
+      <h2>Styling</h2>
+
+      <h1>The Team</h1>
+
+      <img src="" alt="Jason Hargrove"/>
+      <h2>Jason Hargrove</h2>
+      <h3>Software Engineer</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos, neque obcaecati repellat consequatur recusandae quas amet
+      </p>
+      <img src="" alt="LinkedIn"/>
+      <img src="" alt="GitHub"/>
+
+      <img src="" alt="Jessalynn Moncada"/>
+      <h2>Jessalynn Moncada</h2>
+      <h3>Software Engineer</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos, neque obcaecati repellat consequatur recusandae quas amet
+      </p>
+      <img src="" alt="LinkedIn"/>
+      <img src="" alt="GitHub"/>
+
+      <img src="" alt="Katherine Gomez"/>
+      <h2>Katherine Gomez</h2>
+      <h3>UX Designer</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos, neque obcaecati repellat consequatur recusandae quas amet
+      </p>
+      <img src="" alt="LinkedIn"/>
+      <img src="" alt="WordPress"/>
+
+      <h1>The Result</h1>
+
+      {/* ===== Footer goes here. ===== */}
     </div>
   )
 }
