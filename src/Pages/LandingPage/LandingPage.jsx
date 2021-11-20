1a197f539319a7f0e3ad5950f29c8a207921b68f
Jason Hargrove <rijaha@gmail.com>
2021-11-20 14:46:12 -0600

    R&R code in App.css. Added font-family to body selector to make global.

--- src/Pages/LandingPage/LandingPage.jsx
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -4,64 +4,65 @@ import './LandingPage.css';
 
 function LandingPage() {
   return (
-    <div>
+    <>
       <LandingNav />
-      <h1>Unwind With Color</h1>
-      <h2>A Digital Drawing Experience</h2>
-      <button>Go To Project</button>
-      <img src="/img/rainbow-splash-image.svg" alt="rainbow splash"/>
+      <div className="landing-page">
+        <h1>Unwind With Color</h1>
+        <h2>A Digital Drawing Experience</h2>
+        <button>Go To Project</button>
+        <img src="/img/rainbow-splash-image.svg" alt="rainbow splash"/>
 
-      <h1>The Challenge</h1>
+        <h1>The Challenge</h1>
 
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam modi voluptas cum eum doloremque distinctio, non quia eaque cupiditate itaque incidunt soluta repellendus quod perferendis mollitia error tempora dignissimos. Cum.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam modi voluptas cum eum doloremque distinctio, non quia eaque cupiditate itaque incidunt soluta repellendus quod perferendis mollitia error tempora dignissimos. Cum.</p>
 
-      <h1>The Process</h1>
+        <h1>The Process</h1>
 
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At dolorem, maxime ipsam quas, alias labore, nulla eius quibusdam ratione voluptas, ab voluptatibus suscipit? Id, ipsa amet. Fugiat facere, similique commodi.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At dolorem, maxime ipsam quas, alias labore, nulla eius quibusdam ratione voluptas, ab voluptatibus suscipit? Id, ipsa amet. Fugiat facere, similique commodi.</p>
 
-      <img src="" alt="GitHub"/>
-      <img src="" alt="CodeSandbox"/>
+        <img src="" alt="GitHub"/>
+        <img src="" alt="CodeSandbox"/>
 
-      <h1>The Technologies</h1>
+        <h1>The Technologies</h1>
 
-      <img src="" alt="React"/>
-      <img src="" alt="Bootstrap"/>
-      <h2>Front End</h2>
+        <img src="" alt="React"/>
+        <img src="" alt="Bootstrap"/>
+        <h2>Front End</h2>
 
-      <img src="" alt="CSS"/>
-      <img src="" alt="?"/>
-      <h2>Styling</h2>
+        <img src="" alt="CSS"/>
+        <img src="" alt="?"/>
+        <h2>Styling</h2>
 
-      <h1>The Team</h1>
+        <h1>The Team</h1>
 
-      <img src="" alt="Jason Hargrove"/>
-      <h2>Jason Hargrove</h2>
-      <h3>Software Engineer</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos, neque obcaecati repellat consequatur recusandae quas amet
-      </p>
-      <img src="" alt="LinkedIn"/>
-      <img src="" alt="GitHub"/>
+        <img src="" alt="Jason Hargrove"/>
+        <h2>Jason Hargrove</h2>
+        <h3>Software Engineer</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos, neque obcaecati repellat consequatur recusandae quas amet
+        </p>
+        <img src="" alt="LinkedIn"/>
+        <img src="" alt="GitHub"/>
 
-      <img src="" alt="Jessalynn Moncada"/>
-      <h2>Jessalynn Moncada</h2>
-      <h3>Software Engineer</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos, neque obcaecati repellat consequatur recusandae quas amet
-      </p>
-      <img src="" alt="LinkedIn"/>
-      <img src="" alt="GitHub"/>
+        <img src="" alt="Jessalynn Moncada"/>
+        <h2>Jessalynn Moncada</h2>
+        <h3>Software Engineer</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos, neque obcaecati repellat consequatur recusandae quas amet
+        </p>
+        <img src="" alt="LinkedIn"/>
+        <img src="" alt="GitHub"/>
 
-      <img src="" alt="Katherine Gomez"/>
-      <h2>Katherine Gomez</h2>
-      <h3>UX Designer</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos, neque obcaecati repellat consequatur recusandae quas amet
-      </p>
-      <img src="" alt="LinkedIn"/>
-      <img src="" alt="WordPress"/>
+        <img src="" alt="Katherine Gomez"/>
+        <h2>Katherine Gomez</h2>
+        <h3>UX Designer</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos, neque obcaecati repellat consequatur recusandae quas amet
+        </p>
+        <img src="" alt="LinkedIn"/>
+        <img src="" alt="WordPress"/>
 
-      <h1>The Result</h1>
-
+        <h1>The Result</h1>
+      </div>
       {/* ===== Footer goes here. ===== */}
-    </div>
+    </>
   )
 }
 
