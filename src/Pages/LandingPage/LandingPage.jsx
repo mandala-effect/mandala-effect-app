72679c9beb1b0c6231bb3f479c7069385fd018a3
Jason Hargrove <rijaha@gmail.com>
2021-11-20 12:09:13 -0600

    Added splash image, removed react logos, formated LandingNav, LandingP.

--- src/Pages/LandingPage/LandingPage.jsx
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -7,9 +7,9 @@ function LandingPage() {
     return (
         <div>
             <LandingNav />
-            <h1 className="title">Howdy Jason! </h1>
-            <p>this is <i>your</i> masterpiece to design! </p>
-            <img src="https://imgur.com/71FuU1G.gif"alt="coding" />
+            <h1 className="title">Groovy</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis voluptates, officiis assumenda! Dolor qui delectus eos sapiente, modi est doloremque aspernatur, alias odit earum? Dolorum quibusdam officia veritatis, maiores libero.</p>
+            <img src="" alt="coding" />
         </div>
     )
 }
