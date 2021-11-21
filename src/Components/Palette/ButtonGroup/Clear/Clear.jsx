88b8ee22e0f42bb141ed80da2b0e837c1711d396
Jessalynn Moncada <moncadaj@Jessalynns-MacBook-Air.local>
2021-11-20 19:41:00 -0800

    completes palette styling with button group

---
+++ src/Components/Palette/ButtonGroup/Clear/Clear.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Clear.css';
+
+export default function Clear() {
+    return (
+        <div>
+            <button className="clear-btn">Clear Color</button>
+        </div>
+    )
+}
---
+++ src/Components/Palette/ButtonGroup/ClearAll/ClearAll.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './ClearAll.css';
+
+export default function ClearAll() {
+    return (
+        <div>
+            <button className="clearAll-btn">Clear All</button>
+        </div>
+    )
+}
