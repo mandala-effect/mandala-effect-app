e166058bd698483c245812748711d7b31380cb75
Jessalynn Moncada <moncadaj@Jessalynns-MacBook-Air.local>
2021-11-20 17:38:25 -0800

    adds color grid to palette.jsx

---
+++ src/Components/Palette/Head/Head.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Head.css';
+
+export default function Head() {
+    return (
+        <div className="Head">
+            
+        </div>
+    )
+}
---
+++ src/Components/Palette/Palette.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Colors from './Colors/ColorPanel';
+
+export default function Palette() {
+    return (
+        <div className="Palette">
+            <Colors />
+        </div>
+    )
+}
