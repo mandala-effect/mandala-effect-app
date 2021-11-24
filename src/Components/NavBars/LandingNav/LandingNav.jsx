c0e7d47f2051c94ecccf8e0136c3408b208f004e
Jessalynn Moncada <moncadaj@Jessalynns-MacBook-Air.local>
2021-11-23 19:05:39 -0800

    implements an emoji toggle change for night/light mode on primary navbar

--- src/Components/NavBars/LandingNav/LandingNav.jsx
+++ src/Components/NavBars/LandingNav/LandingNav.jsx
@@ -6,6 +6,7 @@ import './LandingNav.css';
 
 export default function LandingNav() {
 	const [darkMode, setDarkMode] = useState(false);
+	const [emoji, setEmoji] = useState(true);
 
 	useEffect(() => {
 		const json = localStorage.getItem("site-dark-mode");
@@ -39,7 +40,11 @@ return (
 					<Nav.Link href="" className="navLink"><Link to="/color">PROJECT</Link></Nav.Link>
 					<Nav.Link href="" className="navLink"><Link to={{path: "https://github.com/mandala-effect/mandala-effect-app" }} target="_blank">GITHUB</Link></Nav.Link>
 					<Nav.Link>
-						<button className="dark-btn" onClick={() => setDarkMode(!darkMode)}>🌙</button>
+						<button className="dark-btn" 
+						onClick={() => {setEmoji(!emoji); setDarkMode(!darkMode)}}>
+								{ emoji ? "🌙  " : " ☀️  " }	
+						</button>
+							{emoji}
 					</Nav.Link>
 				</Nav>
 			</Container>
